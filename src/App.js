8961c61f9eca7f37dae780931de6a96934cba17a
zeexoc <zeexoc@outlook.com>
2021-03-21 09:22:35 +0800

    first push

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, {Component, useEffect,useCallback, useMemo, useState} from 'react'
+import {createEditor, Editor, Transforms,Text} from 'slate'
+import {Slate, Editable, withReact} from 'slate-react'
+
+const App = () => {
+    const editor = useMemo(() => withReact(createEditor()), [])
+    const [value, setValue] = useState([
+        {
+            type: 'paragraph',
+            children: [{ text: 'A line of text in a paragraph.' }],
+        },
+    ])
+
+    const renderElement = useCallback(props => {
+        switch (props.element.type) {
+            case 'code':
+                return <CodeElement {...props} />
+            default:
+                return <DefaultElement {...props} />
+        }
+    }, [])
+
+    // Define a leaf rendering function that is memoized with `useCallback`.
+    const renderLeaf = useCallback(props => {
+        return <Leaf {...props} />
+    }, [])
+
+    return (
+        <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+            <Editable
+                renderElement={renderElement}
+                // Pass in the `renderLeaf` function.
+                renderLeaf={renderLeaf}
+                onKeyDown={event => {
+                    if (!event.ctrlKey) {
+                        return
+                    }
+
+                    switch (event.key) {
+                        case '`': {
+                            event.preventDefault()
+                            const [match] = Editor.nodes(editor, {
+                                match: n => n.type === 'code',
+                            })
+                            Transforms.setNodes(
+                                editor,
+                                { type: match ? null : 'code' },
+                                { match: n => Editor.isBlock(editor, n) }
+                            )
+                            break
+                        }
+
+                        case 'b': {
+                            event.preventDefault()
+                            Transforms.setNodes(
+                                editor,
+                                { bold: true },
+                                { match: n => Text.isText(n), split: true }
+                            )
+                            break
+                        }
+                    }
+                }}
+            />
+        </Slate>
+    )
+}
+
+const Leaf = props => {
+    return (
+        <span
+            {...props.attributes}
+            style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+        >
+      {props.children}
+    </span>
+    )
+}
+
+const CodeElement = props => {
+    return (
+        <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+    )
+}
+
+const DefaultElement = props => {
+    return <p {...props.attributes}>{props.children}</p>
+}
+
+export default App;
